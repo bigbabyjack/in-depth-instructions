ba3468d36fd3206434b9b27cfeedeacc7a68f31f
Jack Conner <1jackconner@gmail.com>
2024-05-12 15:04:20 -0700

    stage done

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,14 @@ async function main() {
     query: `Teach me how to write an arrow function in TypeScript. ${INITIAL_INSTRUCTIONS}`,
   };
 
-  // const generator = new InstructionsGenerator(new LanguageModel('llama3:8b'));
-  //
-  // const instructions = generator.generateInstructions(context);
-  // console.log(instructions);
-  const response = await new LanguageModel('llama3:8b').invoke(new Query(context.query));
-  const responseJson = JSON.parse(response);
-  // console.log(responseJson);
-  const responseText = JSON.parse(response).response
-  for (const step of responseText.split('\n\n')) {
-    console.log("PARSED STEP: ", step);
-  }
+  const generator = new InstructionsGenerator(new LanguageModel('llama3:8b'));
+
+  const instructions = await generator.generateInstructions(context);
+  console.log(instructions);
+  // const response = await new LanguageModel('llama3:8b').invoke(new Query(context.query));
+  // const responseJson = JSON.parse(response);
+  // // console.log(responseJson);
+  // const responseText = JSON.parse(response).response
 
 }
 
